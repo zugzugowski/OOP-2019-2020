ac14bafdf3526db59073db63023180ff9ca4d9f0
skooter500 <skooter500@gmail.com>
2020-02-03 08:18:38 +0000

    week 2

---
+++ java/src/ie/tudublin/HelloProcessing.java
@@ -0,0 +1,40 @@
+package ie.tudublin;
+
+import processing.core.PApplet;
+
+public class HelloProcessing extends PApplet
+{	
+	public void settings()
+	{
+		size(500, 500);
+	}
+
+	public void setup() {
+	}
+
+	/*
+	public void draw() {
+		background(0, 0, 255);
+		stroke(255, 0, 0);
+		fill(255, 255, 0);
+		line(10, 10, 100, 100);		// x1, y1, x2, y2
+		point(50, 50); // x, y
+		ellipse(100, 200, 50, 150); // cx, cy, w, h
+		fill(0, 255, 0);
+		rect(50, 10, 100, 200); // tlx, tly, w, h
+		triangle(10, 10, 100, 90, 200, 200); // vertices	
+	}	
+	*/
+
+	int x = 0; // 4 bytes
+	
+	public void draw()
+	{	
+		
+		background(0);
+		noStroke();
+		fill(255);
+		ellipse(x, mouseY, 50, 50);		
+		x ++;
+	}
+}
--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -2,17 +2,25 @@ package ie.tudublin;
 
 public class Main
 {
+
+    public void helloProcessing()
+	{
+		String[] a = {"MAIN"};
+        processing.core.PApplet.runSketch( a, new HelloProcessing());
+	}
     public static void main(String[] arg)
     {
         System.out.println("Hello world!");
 
         Animal misty = new Dog(); // Polymorphism
-
         misty.setName("Misty");
         System.out.println(misty.getName());  
         misty.speak();  
-
         Cat topcat = new Cat();
         topcat.speak();
+
+        Main main = new Main();
+		main.helloProcessing();
+        
     }
 }
