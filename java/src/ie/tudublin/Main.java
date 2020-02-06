a3e917cc8304b14b7f7cbaf169cc1212b8cb4e16
skooter500 <skooter500@gmail.com>
2020-02-06 09:23:51 +0000

    solution

--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -7,6 +7,12 @@ public class Main
 	{
 		String[] a = {"MAIN"};
         processing.core.PApplet.runSketch( a, new HelloProcessing());
+    }
+    
+    public void bugZap()
+	{
+		String[] a = {"MAIN"};
+        processing.core.PApplet.runSketch( a, new BugZap());
 	}
     
     public static void main(String[] arg)
@@ -23,7 +29,7 @@ public class Main
         */
 
         Main main = new Main();
-		main.helloProcessing();
+		main.bugZap();
         
     }
 }
