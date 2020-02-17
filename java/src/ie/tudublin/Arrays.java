3e8acd217e54be74cffe01a4ecb5ebd0ed3395dc
skooter500 <skooter500@gmail.com>
2020-02-17 11:36:40 +0000

    arrays

---
+++ java/src/ie/tudublin/Arrays.java
@@ -0,0 +1,37 @@
+package ie.tudublin;
+
+import processing.core.PApplet;
+
+public class Arrays extends PApplet
+{	
+	public void settings()
+	{
+		size(500, 500);
+	}
+
+	public void setup() 
+	{
+		
+	}
+
+	
+
+	float offset = 0;
+
+	
+	
+	public void keyPressed()
+	{
+		if (key == ' ')
+		{
+			
+		}
+	}	
+
+
+	public void draw()
+	{	
+		background(0);		
+		colorMode(HSB);	
+	}
+}
