e2e1d23c8fd0129f743f16c6a75c37eb6a616c9d
zugzugowski <bkjz100man@gmail.com>
2020-02-26 14:12:24 +0000

    donedonowski

--- java/src/ie/tudublin/Loops.java
+++ java/src/ie/tudublin/Loops.java
@@ -7,6 +7,10 @@ public class Loops extends PApplet
 	//PFont f;
 
 	int i, j;
+	float diameter = 10;
+	float opacity = 255;
+	float ellipseSize = 20;
+	float squareSize = 25;
 
 	public void settings()
 	{
@@ -66,7 +70,8 @@ public class Loops extends PApplet
 		textAlign(CENTER, CENTER);
 		for(int i = -5 ; i <=5 ; i ++)
 		{
-			float x = map(i, -5, 5, gap, width -gap);				
+			float x = map(i, -5, 5, gap, width -gap);	
+			//System.out.println(x);			
 			line(x, gap, x, height - gap);
 			line(gap, x, width - gap, x);
 			fill(255);
@@ -76,6 +81,8 @@ public class Loops extends PApplet
 		}
 	}
 
+
+
 	
 	public void keyPressed()
 	{
@@ -83,8 +90,81 @@ public class Loops extends PApplet
 		{
 			
 		}
-	}	
+	}
+	
+	public void loop4()
+	{
+		background(100);
+		float gap = width * 0.1f;
+		float halfGap = gap / 2;
+		
+		for(int i = 0; i < 5; i++)
+		{
+			float y = map(i, 0, 5, 50, 8);
+			float x = map(i, 0, 5, 100, 15);
+			float z = map(i, 0, 5, 0, 125);
+			// System.out.println(x);
+			// System.out.println(y);
+			// System.out.println(z);
+			rect(gap + z, halfGap + z, (gap * 8) - (z * 2), halfGap);
+		}
+	}
 
+	public void loops5()
+	{
+		
+		float gap = width * 0.1f;
+		float halfGap = gap * 2;
+		float halfScreen = width * 0.5f;
+		colorMode(HSB);
+		float opacity = 255;
+
+		for( i = 0; i < 25; i++)
+		{	
+			for(int j = 0; j < 25; j++)
+			{
+				float x = map(i, 0, 25, 0, 255);
+				float colors = (x + mouseX) % 255;
+				float posX = map(i, 0, 25, 0, 500);
+				float posY = map(j, 0, 25, 0, 500);		
+				// System.out.println(x);
+				// System.out.println(y);
+				// System.out.println(z);
+				noStroke();
+				fill(colors, 255, 255);
+				ellipse((ellipseSize/2) + posX, (ellipseSize/2) + posY, ellipseSize, ellipseSize);
+			}
+		}
+
+		
+	}
+
+	public void loops6()
+	{
+		float gap = width * 0.1f;
+		float halfGap = gap * 2;
+		float halfScreen = width * 0.5f;
+		colorMode(HSB);
+		float opacity = 255;
+		float z = 0;
+
+		for(int i = 0; i < 25; i++)
+		{
+			for(int j = 0; j < 25; j++)
+			{
+				float x = map(z, 0, 1, 0, 200);
+				//System.out.println(x);
+				//System.out.println(z);
+				float posX = map(i, 0, 25, 0, 500);
+				float posY = map(j, 0, 25, 0, 500);	
+				noStroke();
+				fill(180, 255, x);
+				rect(posX, posY, squareSize, squareSize);
+				z++;
+				z = z % 2;
+			}
+		}
+	}
 
 	public void draw()
 	{	
@@ -94,7 +174,10 @@ public class Loops extends PApplet
 
 		//loops2();
 
-		loops3();
+		//loops3();
+		//loop4();
+		//loops5();
+		loops6();
 	}
 
 	public void shape4()
@@ -115,31 +198,28 @@ public class Loops extends PApplet
 		}
 	}
 
-	int x = -5;
-	int z = 0;
-
-	public void shape5()
-	{
-		background(0);
+	// public void shape5()
+	// {
+	// 	background(0);
 		
 		
 		
-			for(i = 0; i <= 370; i += 40)
-			{
-				for(j = 0; j <= 370; j += 40)
-				{
-					stroke(0, 255, 0);
-					fill(0);
-					rect(55 + i, 60 + j, 40, 40);
-					fill(255);
-					text(x, 70 + i, 50);
-					text(x, 40, 85 + j);			
-				}	
-			}
+	// 		for(i = 0; i <= 370; i += 40)
+	// 		{
+	// 			for(j = 0; j <= 370; j += 40)
+	// 			{
+	// 				stroke(0, 255, 0);
+	// 				fill(0);
+	// 				rect(55 + i, 60 + j, 40, 40);
+	// 				fill(255);
+	// 				text(x, 70 + i, 50);
+	// 				text(x, 40, 85 + j);			
+	// 			}	
+	// 		}
 			
 		
 			
-	}
+	//}
 		
 		
 		
